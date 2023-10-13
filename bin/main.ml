4c3c093cd0ceb861997698b4e6e15f46048bc74e
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-10-13 17:48:25 +0200

    Fixed probability

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,9 @@
-let int_of_bool = function
-  | 0 -> false
-  | _ -> true
-;;
-
 let createBoard size =
   let rec createLine buff maxSize = 
     if List.length buff < maxSize then
-      let random_int = Random.int 2 in
-      createLine (int_of_bool random_int :: buff) maxSize
+      let random_float = Random.float 1.0 in
+      let cell_value = if random_float < 0.3 then true else false in
+      createLine (cell_value :: buff) maxSize
     else
       List.rev buff;
   in
@@ -30,14 +26,15 @@ let displayBoard board =
 in
   let rec displayRow = function
   | [] -> ()
-    | x :: y -> displayCell x; print_newline (); displayRow y
+  | x :: y -> displayCell x; print_char '\n'; displayRow y
 in
-  displayRow board
+  displayRow board;
+  print_newline ()
 
 
 let updateBoard (board : bool list list) : bool list list = 
   let neighbors = [(-1, -1); (-1, 0); (-1, 1);
-                    (0, -1);               (0, 1);
+                    (0, -1);          (0, 1);
                     (1, -1); (1, 0); (1, 1)] in
   let rowLength = List.length (List.nth board 0) in
   let columnLength = List.length board in 
@@ -47,7 +44,7 @@ let updateBoard (board : bool list list) : bool list list =
       if column >= rowLength || row >= columnLength || row < 0 || column < 0 then false else
       List.nth (List.nth board row) column
     in
-    let neighborCount = List.fold_left (fun neighbour (x, y) -> if getElement (row + y) (column + x) then neighbour + 1 else neighbour) 0 neighbors in
+    let neighborCount = List.fold_left (fun neighbour (y, x) -> if getElement (row + y) (column + x) then neighbour + 1 else neighbour) 0 neighbors in
     match (getElement row column, neighborCount) with
     | (false, 3) -> updateCells (true :: buff) row (column + 1)
     | (true, x) when x = 3 || x = 2 -> updateCells (true :: buff) row (column + 1)
@@ -65,9 +62,9 @@ let runGameOfLife boardSize generations =
   let board = createBoard boardSize in
   let rec runGenerations board = function
     | 0 -> ()
-    | x -> displayBoard board; Unix.sleep 1; runGenerations (updateBoard board) (x - 1)
+    | x -> displayBoard board; Unix.sleepf 0.1; runGenerations (updateBoard board) (x - 1)
   in
   runGenerations board generations
 ;;
 
-runGameOfLife 50 100
+runGameOfLife 100 10000
